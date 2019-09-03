565ccbd76ebfd9189f91676734395883272866eb
Greg <greigdillon@gmail.com>
2019-09-02 21:39:04 -0500

    Initial Commit : Added unit test one

---
+++ test/unit/test_1.php
@@ -0,0 +1,10 @@
+<?php
+
+final class TestOne extends \PHPUnit_Framework_TestCase{
+
+
+    //This test whether the value true asserts to True.
+    function testValueForTrue(){
+        $this->assertTrue(true);
+    }
+}
